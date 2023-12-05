a542e579ed92d9a483822524ac3ceaa54452e36c
ghifarit53 <mghifarit53@gmail.com>
2023-12-06 01:18:25 +0700

    cleanup DS_Store files, small tweaks

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,6 @@ use App\Models\Item;
 class HomeController extends Controller
 {
     public function index() {
-        setlocale(LC_MONETARY,"id_ID");
         $items = Item::inRandomOrder()->get();
         $title = 'Home';
 
